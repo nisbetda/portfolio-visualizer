acd9093cd4a3d5bb420d63f68b4cc0fb06b44f9f
Dalton Nisbett <70228149+nisbetda@users.noreply.github.com>
2021-07-08 20:52:21 -0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -6,21 +6,17 @@ symbol_list = ['BTC', 'ETH', 'XLM', 'XMR', 'ALGO', 'ADA']
 #Bitcoin and cardano is .41
 #Bitcoin and ethereum is .64
 #They are the least correlated in terms of btc/eth/xlm/xmr/ada/algo 
-BTC = 0.1999 + 0.024 
-ETH	= 1.999 + 2.857126229
-XMR	= 6.9 
-ADA	= 553.809 
-
-#XMR = 0.39922
-#ADA = 15.580396 + 7
+BTC = 
+ETH	= 
+XMR	= 
+ADA	= 
 
 #Use bitcoin address to update automatically <--------------------------------------------------
 
 #solana (SOL) is Proof of history and stake 
 #On binance
-ALGO = 24.58843
-XLM = 80.69733
+ALGO = 
+XLM = 
 
-SOL = .58142
-
+SOL = 
 
