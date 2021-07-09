33bf325346dc873b13668cf92b3f0f0973ff0b74
Dalton Nisbett <70228149+nisbetda@users.noreply.github.com>
2021-07-08 20:51:06 -0400

    Add files via upload

---
+++ cmc_api_key.py
@@ -0,0 +1,5 @@
+#My CoinMarketCap API Key
+api_key = '0429a3fe-99ec-4adc-8b8f-233777aa60ad'
+
+
+
---
+++ portfolio.py
@@ -0,0 +1,26 @@
+#My Personal Crypto Holdings
+symbols = "BTC,ETH,XLM,XMR,ALGO,ADA"
+symbol_list = ['BTC', 'ETH', 'XLM', 'XMR', 'ALGO', 'ADA']
+
+#Monero and cardano have a statistical correlation of .40 in the past year (7/2020-07/2021)
+#Bitcoin and cardano is .41
+#Bitcoin and ethereum is .64
+#They are the least correlated in terms of btc/eth/xlm/xmr/ada/algo 
+BTC = 0.1999 + 0.024 
+ETH	= 1.999 + 2.857126229
+XMR	= 6.9 
+ADA	= 553.809 
+
+#XMR = 0.39922
+#ADA = 15.580396 + 7
+
+#Use bitcoin address to update automatically <--------------------------------------------------
+
+#solana (SOL) is Proof of history and stake 
+#On binance
+ALGO = 24.58843
+XLM = 80.69733
+
+SOL = .58142
+
+
